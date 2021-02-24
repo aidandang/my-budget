e037827568f2b32695998b1e0eceac8bdcf29297
Aidan Dang <aidandang@gmail.com>
2021-02-24 11:28:20 -0800

    deleted back-end folder

--- back-end/server.js
+++
@@ -1,6 +0,0 @@
-const app = require('./app')
-
-
-// start node.js server on a specified port.
-const port = process.env.PORT || 8000;
-const server = app.listen(port, () => console.log(`Node.js server listening at port: ${port}`));
