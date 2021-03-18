7e4f96014c8e2f54f6c7f4d73652d394c6c2b2e4
Aidan Dang <aidandang@gmail.com>
2021-03-18 15:26:34 -0700

    installed firebase and cset configuration

---
+++ front-end/src/firebase/firebase.utils.js
@@ -0,0 +1,22 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: 'AIzaSyBCacRHx_5qvhxAAvg_w-_o9vf0wKfJFn8',
+  authDomain: 'aidandang-auth.firebaseapp.com',
+  projectId: 'aidandang-auth',
+  storageBucket: 'aidandang-auth.appspot.com',
+  messagingSenderId: '768027804922',
+  appId: '1:768027804922:web:5cbd294dba4691dc23b543',
+  measurementId: 'G-EGENZDK5TH',
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GithubAuthProvider();
+provider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
