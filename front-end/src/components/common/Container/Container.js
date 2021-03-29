2e5eaae45e6b90034271935d47629389d8fb9e3d
Aidan Dang <aidandang@gmail.com>
2021-03-29 09:56:32 -0700

    fixed, tested the header

--- front-end/src/components/common/Container/Container.js
+++ front-end/src/components/common/Container/Container.js
@@ -4,6 +4,8 @@ import { Switch, Route } from 'react-router-dom';
 import { LandingPage } from '../../pages/Landing';
 import { NotFoundPage } from '../../pages/NotFound';
 import { DashboardPage } from '../../pages/Dashboard';
+import { SignInPage } from '../../pages/SignIn';
+import { SignUpPage } from '../../pages/SignUp';
 
 class Container extends Component {
   render() {
@@ -12,6 +14,8 @@ class Container extends Component {
         <Header />
         <Switch>
           <Route path="/" exact component={LandingPage} />
+          <Route path="/sign-in" exact component={SignInPage} />
+          <Route path="/sign-up" exact component={SignUpPage} />
           <Route path="/dashboard" exact component={DashboardPage} />
           <Route path="/" component={NotFoundPage} />
         </Switch>
