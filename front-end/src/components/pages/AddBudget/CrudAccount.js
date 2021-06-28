5c114e1fbdfd7a4ee8155ae781ef4d32af6df999
Aidan Dang <aidandang@gmail.com>
2021-06-28 11:45:58 -0700

    working on Add Budget, add/edit form

--- front-end/src/components/pages/AddBudget/CrudAccount.js
+++ front-end/src/components/pages/AddBudget/CrudAccount.js
@@ -5,10 +5,16 @@ import { reduxForm, Field } from 'redux-form';
 import { Button } from '../../common/Button';
 
 class CrudAccount extends Component {
+  componentDidMount() {
+    this.props.initialize({
+      account: this.props.account ? this.props.account : '',
+    });
+  }
+
   onSubmit = (props) => {};
 
   render() {
-    const { handleSubmit, pristine, submitting, invalid } = this.props;
+    const { handleSubmit, pristine, submitting, invalid, budget } = this.props;
 
     return (
       <Form onSubmit={handleSubmit(this.onSubmit)}>
@@ -19,7 +25,6 @@ class CrudAccount extends Component {
             component={Input}
             label="Account:"
             autoComplete="none"
-            placeholder={this.props.account}
           />
         </div>
         <div>
@@ -29,16 +34,22 @@ class CrudAccount extends Component {
             component={Input}
             label="Budget:"
             autoComplete="none"
-            placeholder={this.props.budget}
+            placeholder={budget ? budget.toString() : '0.00'}
           />
         </div>
         <div className="space--medium">&nbsp;</div>
         <div>
-          <Button type={'submit'} disabled={invalid || submitting || pristine}>
-            Add Budget
+          <Button
+            type={'submit'}
+            disabled={invalid || submitting || pristine}
+            size={'small'}
+          >
+            Add
           </Button>
           <span className="hspace--small">&nbsp;</span>
-          <Button type={'reset'}>Reset</Button>
+          <Button type={'reset'} size={'small'}>
+            Reset
+          </Button>
         </div>
       </Form>
     );
@@ -48,8 +59,8 @@ class CrudAccount extends Component {
 const validate = (values) => {
   const errors = {};
 
-  if (!values.monthyear) {
-    errors.monthyear = 'Required';
+  if (!values.account) {
+    errors.account = 'Required';
   }
 
   return errors;
